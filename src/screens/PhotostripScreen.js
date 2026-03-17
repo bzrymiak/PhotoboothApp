9aec045549524e878d59309192356e36199767dd
Brooke Zrymiak <brookezrymiak@Brooke-5.local>
2026-03-16 18:52:00 -0700

    added intial view shot capacity

--- src/screens/PhotostripScreen.js
+++ src/screens/PhotostripScreen.js
@@ -7,22 +7,59 @@ import {
   StyleSheet,
   Dimensions,
 } from "react-native";
-import { usePhotos } from "../context/PhotoContext";
+import { usePhotos, addPhoto } from "../context/PhotoContext";
+import { captureRef } from "react-native-view-shot";
+import { useRef, useState } from "react";
+import * as MediaLibrary from "expo-media-library";
 
 const NUM_COLUMNS = 1;
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const PHOTO_HEIGHT = SCREEN_WIDTH / 2.1;
 const PHOTO_WIDTH = PHOTO_HEIGHT / 1.1;
 
-export default function GalleryScreen() {
+export default function PhotostripScreen() {
   const { photos, deletePhoto } = usePhotos();
+  const snapshotRef = useRef();
+  const [snapshotImg, setSnapshotImg] = useState();
+  const [mediaPermission, requestMediaPermission] =
+    MediaLibrary.usePermissions();
 
-  if (photos.length === 0) {
+  const snapshot = async () => {
+    try {
+      // save as a jpg to easily save it to user's camera roll
+      const result = await captureRef(snapshotRef, {
+        result: "tmpfile",
+        format: "png",
+      });
+      setSnapshotImg(result);
+      await saveLocally(result);
+    } catch (error) {
+      console.error("Snapshot failed: ", error);
+    }
+  };
+
+  // Save to device camera roll
+  async function saveLocally(photo) {
+    if (!mediaPermission?.granted) {
+      const { granted } = await requestMediaPermission();
+      if (!granted) {
+        console.log("Media library permission not granted");
+        return;
+      }
+    }
+    await MediaLibrary.saveToLibraryAsync(photo); // takes the photo path straight from captureRef
+  }
+
+  const discard = () => {
+    setSnapshotImg(null);
+  };
+
+  if (photos.length < 3) {
     return (
       <View style={styles.emptyContainer}>
         <Text style={styles.emptyText}>No photos yet.</Text>
         <Text style={styles.emptySubText}>
-          Take a photo and save it to the app!
+          Take more photos to create a photostrip!
         </Text>
       </View>
     );
@@ -30,29 +67,48 @@ export default function GalleryScreen() {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={photos}
-        style={styles.shadow}
-        keyExtractor={(_, index) => index.toString()}
-        numColumns={NUM_COLUMNS}
-        renderItem={({ item, index }) => (
-          <View style={styles.photoWrapper}>
-            <Image
-              source={{ uri: item }}
-              style={styles.photo}
-              resizeMode="cover"
-            />
-          </View>
-        )}
-      />
+      {/* debugging snapshot code */}
+      {/* {snapshotImg ? <Text>Snapshot taken</Text> : <Text>No snapshot</Text>}
+      {snapshotImg ? (
+        <View style={styles.snapshotContainer}>
+          <Image
+            resizeMode="contain"
+            style={styles.snapshotImg}
+            source={{ uri: snapshotImg }}
+          />
+        </View>
+      ) : (
+        <View />
+      )} */}
+
+      <View ref={snapshotRef} collapsable={false}>
+        <FlatList
+          data={photos}
+          style={styles.shadow}
+          keyExtractor={(_, index) => index.toString()}
+          numColumns={NUM_COLUMNS}
+          renderItem={({ item, index }) => (
+            <View style={styles.photoWrapper}>
+              <Image
+                source={{ uri: item }}
+                style={styles.photo}
+                resizeMode="cover"
+              />
+            </View>
+          )}
+        />
+      </View>
 
       {/* Bottom action buttons */}
       <View style={styles.previewActions}>
-        <TouchableOpacity style={styles.actionButton}>
+        <TouchableOpacity style={styles.actionButton} onPress={discard}>
           <Text style={styles.actionButtonText}>Discard</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={[styles.actionButton, styles.localButton]}>
+        <TouchableOpacity
+          style={[styles.actionButton, styles.localButton]}
+          onPress={snapshot}
+        >
           <Text style={styles.actionButtonText}>Save Photostrip</Text>
         </TouchableOpacity>
       </View>
@@ -144,5 +200,12 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     gap: 24,
     paddingHorizontal: 30,
+  },
+
+  snapshotImg: {
+    width: PHOTO_WIDTH,
+    height: PHOTO_HEIGHT * 3,
+    position: "absolute",
+    zIndex: 1,
   },
 });
