00fbed60c2aa7cd803e7bf5b1482f1e3f1d03531
faithleungg <faithleung28@gmail.com>
2026-04-07 09:07:53 -0700

    changed profile placeholder color

--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -222,7 +222,7 @@ const styles = StyleSheet.create({
     width: 120,
     height: 120,
     borderRadius: 60,
-    backgroundColor: "#ffffff",
+    backgroundColor: "#ccc",
   },
   editBtn: {
     backgroundColor: "#000",
